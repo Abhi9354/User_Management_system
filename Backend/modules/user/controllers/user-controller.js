02b6b777355b51afe8dd283cb0b8e5f466e099ed
knowledge <abhi7ty4@gmail.com>
2024-05-03 23:39:48 +0530

    comment

--- Backend/modules/user/controllers/user-controller.js
+++ Backend/modules/user/controllers/user-controller.js
@@ -1,29 +1,29 @@
-import { userService } from "../services/user-service.js";
+// import { userService } from "../services/user-service.js";
 
-export const register = async (req, res) => {
-  const data = req.body;
+// export const register = async (req, res) => {
+//   const data = req.body;
 
-  try {
-    const doc = await userService.register(data);
-    if(doc._id){
-        console.log('doc ',doc);
-        res.json({"message":"success","doc":doc})
-    }
+//   try {
+//     const doc = await userService.register(data);
+//     if(doc._id){
+//         console.log('doc ',doc);
+//         res.json({"message":"success","doc":doc})
+//     }
 
-    else{
-        res.json({"message":"fail"})
-    }
-  } catch (err) {
-    console.log("err",err);
-    throw new error(err);
-  }
-};
-export const login = (req, res) => {
-  res.send("login");
-};
-export const profile = (req, res) => {
-  res.send("profile");
-};
-export const remove = (req, res) => {
-  res.send("remove");
-};
+//     else{
+//         res.json({"message":"fail"})
+//     }
+//   } catch (err) {
+//     console.log("err",err);
+//     throw new error(err);
+//   }
+// };
+// export const login = (req, res) => {
+//   res.send("login");
+// };
+// export const profile = (req, res) => {
+//   res.send("profile");
+// };
+// export const remove = (req, res) => {
+//   res.send("remove");
+// };
